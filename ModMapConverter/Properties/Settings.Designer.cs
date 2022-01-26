0cbb05e2e1168e42161770f4da779b0884b404b8
haawwkeye <CONNORBISHOP53@GMAIL.COM>
2022-01-26 18:18:58 -0500

    bump version

--- ModMapConverter/Properties/Settings.Designer.cs
+++ ModMapConverter/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace ModMapConverter.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("4.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("4.4")]
         public float Version {
             get {
                 return ((float)(this["Version"]));
