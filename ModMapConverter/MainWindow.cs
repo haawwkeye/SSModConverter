9dc0300c3f902252536628f377d40ce9db3471a7
haawwkeye <connorbishop53@gmail.com>
2022-01-30 20:49:33 -0500

    verify test

--- ModMapConverter/MainWindow.cs
+++ ModMapConverter/MainWindow.cs
@@ -325,7 +325,7 @@ namespace ModMapConverter
 				{
 				/**/
 					isConvertingMap = true;
-					//MessageBox.Show("Sound Space JSON files not supported yet.", "Error");
+					MessageBox.Show("Sound Space JSON files not supported yet.", "Error");
 					string path = DownloadHandler.BSHandler.path + "\\map.json";
 
 					File.WriteAllText(path, text);
